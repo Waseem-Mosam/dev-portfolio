189780c0edea7d3e7d4e85c89fed378ef2264db0
Waseem-Mosam <waseem.mosam1@gmail.com>
2025-11-20 00:32:17 +0200

    config: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -77,6 +78,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${ibmPlexMono.variable} antialiased`}
 			>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
